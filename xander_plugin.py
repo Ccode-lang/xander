617670cb5dc58ff83a269f6313eaccce8f48ed8b
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2023-10-21 18:46:15 -0700

    Voice support for one server

--- xander_plugin.py
+++ xander_plugin.py
@@ -1,5 +1,6 @@
 import __main__
 
+# bot.py keywords
 config = __main__.config
 log = __main__.log
 discord = __main__.discord
@@ -7,8 +8,24 @@ modlog = __main__.modlog
 client = __main__.client
 help_menu = __main__.help_menu
 
+# Library state variables
+voice_client = None
+
+
+# Plugin utility methods
 def getother(name):
     return getattr(__main__, name)
 
 def help_menu_edit(feature_name, info):
     help_menu[feature_name] = info
+
+async def join_vc(channel):
+    global voice_client
+    if voice_client == None:
+        voice_client = await channel.connect()
+
+async def leave_vc():
+    global voice_client
+    if not voice_client == None:
+        await voice_client.disconnect()
+        voice_client = None
