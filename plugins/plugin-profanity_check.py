2c367edc2be818f3c2de3cf9c9a58a184468051e
Ccode-lang <coop112097@gmail.com>
2024-01-01 18:22:16 -0700

    First test

--- plugins/plugin-profanity_check.py
+++ plugins/plugin-profanity_check.py
@@ -1,17 +1,14 @@
-from better_profanity import profanity
+from profanity_check import predict, predict_prob
 from xander_plugin import *
 def onload():
     help_menu_edit("Profanity check plugin", "A plugin that filters profanity.")
     log("Profanity plugin loaded!")
 
 async def onmessage_priority(message):
-    check = "".join(ch for ch in message.content if ch.isalnum())
-
-    if profanity.contains_profanity(check) or profanity.contains_profanity(message.content):
+    if predict(message.content):
         log(message.author.name + ' said: "' + message.content +
             '" on server "' + str(message.guild) + '"')
         await modlog(message.author.name + ' said: "' + message.content + '" on server "' + message.guild.name + '"', message, True)
-
         return False
     return True
 
