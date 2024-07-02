7fde8bbf57d8118d00a362e577af7fb1e9e5a003
Cooper <c112097@yahoo.com>
2024-07-01 20:50:04 -0400

    use better API hook for delete log

--- plugins/plugin-delete_log.py
+++ plugins/plugin-delete_log.py
@@ -4,9 +4,10 @@ def onload():
     help_menu_edit("Delete log plugin", "A plugin that logs everything that gets deleted.")
     log("Delete log plugin loaded!")
 
-async def onrawmessagedelete(rawmessage):
-    if not rawmessage.cached_message is None and not rawmessage.cached_message.author.id == client.user.id:
-        await modlog(f"{rawmessage.cached_message.author.display_name} deleted their message: \"{rawmessage.cached_message.content}\"", rawmessage.cached_message)
+async def onmessagedelete(message):
+    if not message.author.id == client.user.id:
+        await modlog(f"message written by {message.author.display_name} was deleted: \"{message.content}\"", message)
+    return True
 
 
 def onexit():
